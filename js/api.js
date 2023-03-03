4eb81788d5cbfba5436b647d7f992957d31cff4e
Tanvir Ahmad <tanvirahmadstudent@gmail.com>
2023-03-03 09:06:59 +0600

    added more details section using modal

--- js/api.js
+++ js/api.js
@@ -36,5 +36,58 @@ const displayAllData = allData =>{
         allAiCardsContainer.appendChild(dataDiv);
     }
 }
-
+const loadSingle =async id =>{
+    const url = `https://openapi.programming-hero.com/api/ai/tool/${id<10?'0'+id:id}`;
+    const res = await fetch(url);
+    const data = await res.json();
+    showSingleDetails(data.data);
+}
+const showSingleDetails = data =>{
+    const detailsContainer = document.getElementById('details-container');
+    console.log(data);
+    detailsContainer.innerHTML = `
+       
+        <div class="grid grid-cols-2 gap-5 p-5">
+           <div class="bg-red-50 rounded-lg shadow-xl p-3">
+               <h1 class="text-lg font-semibold">${data.description}</h1>
+               <div class="grid grid-cols-3 gap-3 mt-3">
+                  <div class="bg-stone-50 p-3">
+                     <h1 class="text-center text-xl font-semibold text-green-400">${data.pricing[0].price} <br> ${data.pricing[0].plan}</h1>
+                  </div>
+                  <div class="bg-stone-50 p-3">
+                     <h1 class="text-center text-xl font-semibold text-orange-400">${data.pricing[1].price} <br> ${data.pricing[1].plan}</h1>
+                  </div>
+                  <div class="bg-stone-50 p-3">
+                     <h1 class="text-center text-xl font-semibold text-red-400">${data.pricing[2].price} <br> ${data.pricing[2].plan}</h1>
+                  </div>
+               </div>
+               <div class="flex gap-3 mt-5">
+                   <div>
+                        <h1 class="text-xl font-semibold">Features</h1>
+                        <ul class="list-disc ml-5">
+                           <li>${data.features[1].feature_name}</li>
+                           <li>${data.features[2].feature_name}</li>
+                           <li>${data.features[3].feature_name}</li>
+                        </ul>
+                   </div>
+                   <div>
+                        <h1 class="text-xl font-semibold">Integrations</h1>
+                        <ul class="list-disc ml-5">
+                            <li>${data.integrations[0]}</li>
+                            <li>${data.integrations[1]}</li>
+                            <li>${data.integrations[2]}</li>
+                        </ul>
+                   </div>
+               </div>
+           </div>
+           <div class="shadow-xl p-3 rounded-lg">
+              <figure><img src="${data.image_link[0]}" alt="Album"/></figure>
+              <h1 class="text-center text-lg font-semibold">${data.input_output_examples[0].input}</h1>
+              <h1 class="text-center text-lg">${data.input_output_examples[0].output}</h1>
+           </div>
+           
+        </div>
+       
+    `;
+}
 loadAllData();
